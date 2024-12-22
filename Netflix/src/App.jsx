7228b46dac301523fcbe0945cf9ba833de0c3f61
Wajiha-Hashmi <ziashah6822@gmail.com>
2024-12-23 00:40:14 +0500

    commit it

--- Netflix/src/App.jsx
+++ Netflix/src/App.jsx
@@ -1,5 +1,15 @@
+import React from "react";
+import "./index.css"; // Ensure this is at the top of your file
+import Body from "./components/Body";
+import { Toaster } from "react-hot-toast";
+
 function App() {
-  return <div className="App">Let's build netflix</div>;
+  return (
+    <div>
+      <Body />
+      <Toaster />
+    </div>
+  );
 }
 
 export default App;
