6f44c792d3a8668a5a34615b2d2409bad01bd954
Wajiha-Hashmi <ziashah6822@gmail.com>
2024-12-23 00:37:34 +0500

    commit utils

---
+++ Netflix/src/utils/constant.js
@@ -0,0 +1,14 @@
+export const API_END_POINT = "http://localhost:3000/api/v1/user";
+export const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MDZjYjU2ZWJhMjI0ZTg4ZDAwMGFkODk1YzRlMzNmZCIsIm5iZiI6MTczNDg2NzI3Ny4yNzksInN1YiI6IjY3NjdmOTRkODdiOTJhNDNiZTVlNDI3YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AhoOBz8JdkvkK1fxOPkssqpHoBwvBMfZYdJuEqE_wVc",
+  },
+};
+export const Now_Playing_Movie =
+  "https://api.themoviedb.org/3/movie/now_playing";
+export const Popular_Movie = "https://api.themoviedb.org/3/movie/popular";
+export const Top_Rated_Movie = "https://api.themoviedb.org/3/movie/top_rated";
+export const Upcoming_Movie = "https://api.themoviedb.org/3/movie/upcoming";
